e8415335cae99a7a5fe37a6735e8898b64bf9496
wasim22 <wasimghumro@hotmail.com>
2014-05-06 14:45:46 -0500

    Created project

---
+++ OurFirstVSProject/JavaScript.js
@@ -0,0 +1,13 @@
+﻿function LongestWord(sen) {
+    var wordsArray = sen.split(" ");
+    for (var i = 0; i < wordsArray.length; i++) {
+        lengthArray[i] = wordsArray[i].length;
+    }
+    for (var j = 0; j < lengthArry.length; j++) {
+        lengthArray[i] = wordsArray[i].length;
+    }
+    return sen;
+
+}
+
+LongestWord("This is a sentence");
